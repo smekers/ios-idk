9c7b9e72bf1f9dd74596b05c68c6992df9029810
smekers <150516551+smekers@users.noreply.github.com>
2024-07-31 16:48:40 +0800

    create new project

---
+++ IOS-idk/IOS-idk/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  IOS-idk
+//
+//  Created by Rachel Castor on 7/31/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
